897e1171dc8bd9ce86d0a350a7cfaf5f24847c95
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-30 19:03:27 +0530

    Welcome message

---
+++ UserRegistration.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistration {
+
+        public static void main(String[] args) {
+                System.out.println("Welcome in User Registration program");
+        }
+}
+
