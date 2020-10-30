dd8593a1692c10e2e556ff6a05ca5efa13bf04b9
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-30 19:18:02 +0530

    Valid LastName

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,6 +6,7 @@ public class UserRegistration {
                 System.out.println("Welcome in User Registration program");
 		UserRegistration user = new UserRegistration();
 		user.firstname();
+		user.lastname();
         }
 
 	public boolean firstname() {
@@ -15,6 +16,14 @@ public class UserRegistration {
 		System.out.println("First Name is:" + name + "\nvalid: " + valid);
 		return valid;
 
+	}
+
+	public boolean lastname() {
+		System.out.println("Enter Lastname:");
+		String name = sc.next();
+		boolean valid = name.matches("^[A-Z][a-z]{3,}");
+		System.out.println("Last Name is:" + name + "\nvalid: " + valid);
+		return valid;
 	}
 }
 
